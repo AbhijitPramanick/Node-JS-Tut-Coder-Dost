ab06a3a109166e1272ded07e2b8640792dfe91ff
AbhijitPramanick <amanik1995@gmail.com>
2023-12-07 15:50:28 +0530

    Node JS core modules, npm, packageJson ESmodules commonJS import export modules

---
+++ index.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+// const lib = require("./lib");
+// import { sum, diff } from "./lib.js";
+
+// const a = 5, b = 2;
+
+// console.log(`The sum is : ${sum(a, b)}`);
+// console.log(`The diff is : ${diff(a, b)}`);
+
+// const t1 = performance.now();
+// const text = fs.readFileSync('demo.txt', 'utf-8');
+// console.log(text);
+// const t2 = performance.now();
+// console.log(`Time : ${t2 - t1} milliseconds`);
+
+
+
+// fs.readFile('demo.txt', 'utf-8', (err, txt) => {
+//     console.log(txt);
+// })
---
+++ lib.js
@@ -0,0 +1,12 @@
+// const sum = (a, b) => a + b;
+
+// const diff = (a, b) => a - b;
+
+// export { sum, diff };
+const sum = function (a, b) {
+    return a + b;
+}
+const diff = function (a, b) {
+    return a - b;
+}
+export { sum, diff };
