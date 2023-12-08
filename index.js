52c3c8cc52ebacc136764440f1859958de474bf3
AbhijitPramanick <amanik1995@gmail.com>
2023-12-08 13:39:50 +0530

    Chapter 1 with assignments completed

--- index.js
+++ index.js
@@ -1,21 +1,50 @@
-const fs = require('fs');
+// // Assingment 2:
+// // index.js
 
-// const lib = require("./lib");
-// import { sum, diff } from "./lib.js";
+// // The first two elements in process.argv are the Node.js executable and the script file path.
+// // Actual command line arguments start from index 2.
+// // const args = process.argv.slice(2);
+// const args = process.argv;
 
-// const a = 5, b = 2;
+// console.log('Command line arguments:', args);
 
-// console.log(`The sum is : ${sum(a, b)}`);
-// console.log(`The diff is : ${diff(a, b)}`);
+// // Now you can use args[0], args[1], etc., in your program.
 
-// const t1 = performance.now();
-// const text = fs.readFileSync('demo.txt', 'utf-8');
-// console.log(text);
-// const t2 = performance.now();
-// console.log(`Time : ${t2 - t1} milliseconds`);
+////////////////////////////
 
+// //Assignment 5
+// // index.js
 
+// const { exec } = require("child_process");
+// const { stderr } = require("process");
 
-// fs.readFile('demo.txt', 'utf-8', (err, txt) => {
-//     console.log(txt);
+// exec("dir", (err, stdout, stderr) => {
+//     if (err) {
+//         console.log(`There is some error : ${err.message}`);
+//         return;
+//     }
+//     console.log(`The folowing info was found : ${stdout}`);
 // })
+
+/////////////////////////////
+// Assignment 3 : Understanding OS module
+
+// os_info.js
+
+// Import the 'os' module, which is built into Node.js and provides information about the operating system.
+const os = require('os');
+
+// Print the operating system platform (e.g., 'win32', 'linux', 'darwin' for Windows, Linux, and macOS).
+console.log('Platform:', os.platform());
+
+// Print the CPU architecture (e.g., 'x64', 'arm' for 64-bit and ARM architectures).
+console.log('Architecture:', os.arch());
+
+// Print information about CPUs, such as their model, speed, and usage.
+console.log('CPUs:', os.cpus());
+
+// Print the total system memory in bytes.
+console.log('Total Memory:', os.totalmem());
+
+// Print the free system memory in bytes.
+console.log('Free Memory:', os.freemem());
--- lib.js
+++ lib.js
@@ -1,12 +1,4 @@
-// const sum = (a, b) => a + b;
+const sum = (a, b) => a + b;
+const diff = (a, b) => a - b;
 
-// const diff = (a, b) => a - b;
-
-// export { sum, diff };
-const sum = function (a, b) {
-    return a + b;
-}
-const diff = function (a, b) {
-    return a - b;
-}
 export { sum, diff };
