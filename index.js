1ea5be52db9d1d5a4deb0a30e97e7f77a2a67f73
AbhijitPramanick <amanik1995@gmail.com>
2023-12-09 07:56:04 +0530

    Rendering an Index html by nodeJS http server

--- index.js
+++ index.js
@@ -1,50 +1,21 @@
-// // Assingment 2:
-// // index.js
+const http = require('http');
+const fs = require('fs');
 
-// // The first two elements in process.argv are the Node.js executable and the script file path.
-// // Actual command line arguments start from index 2.
-// // const args = process.argv.slice(2);
-// const args = process.argv;
-
-// console.log('Command line arguments:', args);
-
-// // Now you can use args[0], args[1], etc., in your program.
-
-////////////////////////////
-
-// //Assignment 5
-// // index.js
-
-// const { exec } = require("child_process");
-// const { stderr } = require("process");
-
-// exec("dir", (err, stdout, stderr) => {
-//     if (err) {
-//         console.log(`There is some error : ${err.message}`);
-//         return;
-//     }
-//     console.log(`The folowing info was found : ${stdout}`);
-// })
-
-/////////////////////////////
-// Assignment 3 : Understanding OS module
-
-// os_info.js
-
-// Import the 'os' module, which is built into Node.js and provides information about the operating system.
-const os = require('os');
-
-// Print the operating system platform (e.g., 'win32', 'linux', 'darwin' for Windows, Linux, and macOS).
-console.log('Platform:', os.platform());
-
-// Print the CPU architecture (e.g., 'x64', 'arm' for 64-bit and ARM architectures).
-console.log('Architecture:', os.arch());
-
-// Print information about CPUs, such as their model, speed, and usage.
-console.log('CPUs:', os.cpus());
-
-// Print the total system memory in bytes.
-console.log('Total Memory:', os.totalmem());
-
-// Print the free system memory in bytes.
-console.log('Free Memory:', os.freemem());
+const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+const index = fs.readFileSync('index.html', 'utf-8');
+const server = http.createServer((req, res) => {
+    if (req.url.startsWith('/product')) {
+        res.setHeader('Content-Type', 'text/html');
+        const products = data.products;
+        const id = req.url.split('/')[2];
+        const product = products.find(p => p.id === +id);
+        console.log(product)
+        const modifiedIndex = index
+            .replace('**title**', product.title)
+            .replace('**url**', product.thumbnail)
+            .replace('**price**', product.price)
+            .replace('**rating**', product.rating);
+        res.end(modifiedIndex);
+    }
+})
+server.listen(8080);
