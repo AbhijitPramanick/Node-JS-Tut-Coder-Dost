3722598e022f0cf2e61a97694d51ef403d97ded6
AbhijitPramanick <amanik1995@gmail.com>
2023-12-09 10:51:00 +0530

    Middlewares, logger

--- index.js
+++ index.js
@@ -1,21 +1,48 @@
-const http = require('http');
-const fs = require('fs');
+const express = require('express');
+const server = express();
 
+const fs = require('fs');
 const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
-const index = fs.readFileSync('index.html', 'utf-8');
-const server = http.createServer((req, res) => {
-    if (req.url.startsWith('/product')) {
-        res.setHeader('Content-Type', 'text/html');
-        const products = data.products;
-        const id = req.url.split('/')[2];
-        const product = products.find(p => p.id === +id);
-        console.log(product)
-        const modifiedIndex = index
-            .replace('**title**', product.title)
-            .replace('**url**', product.thumbnail)
-            .replace('**price**', product.price)
-            .replace('**rating**', product.rating);
-        res.end(modifiedIndex);
-    }
+
+//Middlewares
+
+//Logger-middleware
+server.use((req, res, next) => {
+    console.log(req.url, req.method, req.ip, req.hostname, new Date(), req.get('User-Agent'));
+    next();
 })
-server.listen(8080);
+
+////////////////////////////
+//API / Endpoint - Route
+server.get('/', (req, res) => {
+    res.json({ type: 'GET' });
+})
+server.post('/', (req, res) => {
+    res.json({ type: 'POST' });
+})
+server.put('/', (req, res) => {
+    res.json({ type: 'PUT' });
+})
+server.patch('/', (req, res) => {
+    res.json({ type: 'PATCH' });
+})
+server.delete('/', (req, res) => {
+    res.json({ type: 'DELETE' });
+})
+////////////////////////////
+server.get("/demo", (req, res) => {
+    // res.send('<h1>Hello</h1>');
+
+    // path must be absolute or specify root to res.sendFile
+    // res.sendFile('E:\\Documents\\Campusing bootcamp\\NodeJS_coderDost\\index.html');
+
+    // res.json(data);
+
+    // res.sendStatus(404);
+
+    // res.status(201).send('<h1>Hello</h1>');
+})
+
+server.listen(8080, () => {
+    console.log(`Listening to port 8080`);
+})
