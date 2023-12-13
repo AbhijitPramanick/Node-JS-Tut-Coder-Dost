484ab24aae3a354a8815c34fae92b9a57966f41b
AbhijitPramanick <amanik1995@gmail.com>
2023-12-13 23:26:45 +0530

    MVC controllers

--- index.js
+++ index.js
@@ -5,54 +5,54 @@ const port = 8080;
 const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
 const products = data.products;
 
-server.use(express.json());
-
-server.get('/products', (req, res, next) => {
-    res.json(products);
-});
-
-//Read GET /products/:id
-server.get('/products/:id', (req, res) => {
-    const prodId = +req.params.id;
-    const product = products.find(p => p.id === prodId);
-    res.json(product);
-});
-
-//Create POST /products
-server.post('/products', (req, res) => {
+//Controllers
+const createProduct = (req, res) => {
     const product = req.body;
     console.log(product);
     products.push(product);
     res.json(product);
-});
+}
+const getAllProducts = (req, res, next) => {
+    res.json(products);
+}
+const getProduct = (req, res) => {
+    const prodId = +req.params.id;
+    const product = products.find(p => p.id === prodId);
+    res.json(product);
+}
 
-//Update PUT /products/:id
-server.put("/products/:id", (req, res) => {
+const replaceProduct = (req, res) => {
     const id = +req.params.id;
     const productIndex = products.findIndex(p => p.id === id);
     products.splice(productIndex, 1, { ...req.body, id: id });
     res.status(201).json();
-})
+}
 
-//Update PATCH /products/:id    
-server.patch("/products/:id", (req, res) => {
+const updateProduct = (req, res) => {
     const id = +req.params.id;
     const productIndex = products.findIndex(p => p.id === id);
     const product = products[productIndex];
     products.splice(productIndex, 1, { ...product, ...req.body })
     res.status(201).json();
-})
-
-// Delete DELETE /products/:id
-server.delete("/products/:id", (req, res) => {
+}
+const deleteProduct = (req, res) => {
     const id = +req.params.id;
     const productIndex = products.findIndex(p => p.id === id)
     const product = products[productIndex]; //For sending the product deleted as response to show on the client side about the deleted product
     products.splice(productIndex, 1);
     res.status(200).json(product);
-})
+}
+server.use(express.json());
+
+server
+    .get('/products', getAllProducts)
+    .get('/products/:id', getProduct)
+    .post('/products', createProduct)
+    .put("/products/:id", replaceProduct)
+    .patch("/products/:id", updateProduct)
+    .delete("/products/:id", deleteProduct);
 
 
 server.listen(port, () => {
     console.log(`Listening to port ${port}`);
-})
+}) 
