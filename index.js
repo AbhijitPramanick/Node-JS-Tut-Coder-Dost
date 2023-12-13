ac772fab8436420a433123e8289998f941242827
AbhijitPramanick <amanik1995@gmail.com>
2023-12-13 18:52:21 +0530

    CRUD operation in REST API, GET, POST, PUT, PATCH, DELETE

--- index.js
+++ index.js
@@ -1,22 +1,58 @@
 const express = require('express');
 const server = express();
+const fs = require('fs');
+const port = 8080;
+const data = JSON.parse(fs.readFileSync('data.json', 'utf-8'));
+const products = data.products;
 
-//Another built-in middlware
-//third party middlewares
+server.use(express.json());
 
-const morgan = require('morgan');
+server.get('/products', (req, res, next) => {
+    res.json(products);
+});
 
-// server.use(morgan('dev'));
-// server.use(morgan('default'));
-server.use(morgan('tiny'));
+//Read GET /products/:id
+server.get('/products/:id', (req, res) => {
+    const prodId = +req.params.id;
+    const product = products.find(p => p.id === prodId);
+    res.json(product);
+});
 
-//Logger middleware
-// server.get('/', (req, res, next) => {
-//     console.log(`request url : ${req.url},\nRequest Hostname : ${req.hostname},\nRequest IP : ${req.ip},\nDate : ${new Date()},\nRequest.method : ${req.method},\nOther details : ${req.get('User-Agent')}`);
-//     res.send('Hello');
-//     next();
-// })
+//Create POST /products
+server.post('/products', (req, res) => {
+    const product = req.body;
+    console.log(product);
+    products.push(product);
+    res.json(product);
+});
 
-server.listen(8080, () => {
-    console.log(`Listening to port 8080`);
+//Update PUT /products/:id
+server.put("/products/:id", (req, res) => {
+    const id = +req.params.id;
+    const productIndex = products.findIndex(p => p.id === id);
+    products.splice(productIndex, 1, { ...req.body, id: id });
+    res.status(201).json();
+})
+
+//Update PATCH /products/:id    
+server.patch("/products/:id", (req, res) => {
+    const id = +req.params.id;
+    const productIndex = products.findIndex(p => p.id === id);
+    const product = products[productIndex];
+    products.splice(productIndex, 1, { ...product, ...req.body })
+    res.status(201).json();
+})
+
+// Delete DELETE /products/:id
+server.delete("/products/:id", (req, res) => {
+    const id = +req.params.id;
+    const productIndex = products.findIndex(p => p.id === id)
+    const product = products[productIndex]; //For sending the product deleted as response to show on the client side about the deleted product
+    products.splice(productIndex, 1);
+    res.status(200).json(product);
+})
+
+
+server.listen(port, () => {
+    console.log(`Listening to port ${port}`);
 })
